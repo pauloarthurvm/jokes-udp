79f9f5aa31214576db08cf0192d5b2fbe8f85180
Paulo Varassin <pauloarthurvm@hotmail.com>
2023-08-14 20:12:48 -0300

    Enhance Client code

--- src/main/java/udpclient/UDPClient.java
+++ src/main/java/udpclient/UDPClient.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class UDPClient {
 
@@ -19,24 +20,33 @@ public class UDPClient {
 
     public static void main(String[] args) {
 
-//        byte[] buffer = new byte[512];
-        final byte[] bufferRequest = "Make me laugh...".getBytes();
-        final byte[] bufferResponse = new byte[512];
+        sendPacket();
+        String quote = receivePacket();
+        System.out.println(quote);
 
+    }
+
+    private static String receivePacket() {
+        final byte[] bufferResponse = new byte[512];
+        try {
+            dgPacketResponse = new DatagramPacket(bufferResponse, bufferResponse.length);
+            dgSocket.receive(dgPacketResponse);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new String(bufferResponse, 0, dgPacketResponse.getLength());
+    }
+
+    private static void sendPacket() {
+        final byte[] bufferRequest = "Make me laugh...".getBytes();
         try {
             address = InetAddress.getLocalHost();
             dgPacketRequest = new DatagramPacket(bufferRequest, bufferRequest.length, address, port);
             address = InetAddress.getByName("localhost");
             dgSocket = new DatagramSocket();
             dgSocket.send(dgPacketRequest);
-            dgPacketResponse = new DatagramPacket(bufferResponse, bufferResponse.length);
-            dgSocket.receive(dgPacketResponse);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        String quote = new String(bufferResponse, 0, dgPacketResponse.getLength());
-        System.out.println(quote);
-
     }
 }
